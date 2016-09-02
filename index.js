e4350efa39e6076ce20a019d2d9fe6441809e615
沈志伟 <rayshen1012@gmail.com>
2016-09-02 21:09:10 +0800

    add image dowload function

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var superagent = require('superagent');
 var cheerio = require('cheerio');  
 var async = require('async');
 var fs= require('fs');
+var request = require("request");
+var http = require("http");
+
+
+//判断文件夹是否存在
+fs.exists("img/", function(exists) {
+    if(!exists){
+        fs.mkdirSync("img/");
+    }
+});
 
 start();
 
@@ -20,17 +30,17 @@ function start(){
         });
 
         //并发遍历heroes对象
-        async.mapLimit(heroes,5, 
+        async.mapLimit(heroes,1,
             function (heroUrl, callback) {
             // 对每个角色对象的处理逻辑
                 fetchInfo(heroUrl, callback);
-            }, 
+            },
             function (err, result) {
                 if(err){
                     console.log("error is:"+err);
                 }
                 //这里的result就是callback回来的数组
-                console.log("抓取结束，共计:"+result.length+"个");  
+                console.log("抓取结束，共计:"+result.length+"个");
                 result.forEach(function(hero){
                     var resultString = JSON.stringify(hero);
                     console.log(resultString);
@@ -42,7 +52,6 @@ function start(){
                 });
             }
         );
-
         //串行遍历heroes对象
         // async.mapSeries(heroes,function (heroUrl, callback) {
         //     // 对每个角色对象的处理逻辑
@@ -87,17 +96,64 @@ function fetchInfo(heroUrl, callback){
                 var heroTitle = $('.hero-title').first().text();
                 var heroName = $('.hero-name').first().text();
                 var heroType = $('.hero-tag').first().text()+" "+$('.hero-tag').last().text();
-                console.log('找到英雄:'+heroTitle+" "+heroName+"|"+heroType);
-                concurrencyCount--;
-                var hero = {
-                    succ:true,
-                    title:heroTitle,
-                    name:heroName,
-                    type:heroType,
-                    url:heroUrl
+                var heroesPicUrlArray = new Array();
+                $(".ui-slide__panel img").each(function(i, e) {
+                    heroesPicUrlArray.push($(e).attr("src"));
+                });
+                console.log(heroesPicUrlArray);
+                var index = 0;
+                async.mapLimit(heroesPicUrlArray,heroesPicUrlArray.length,
+                    function (heroesPicUrl, downloadCallback) {
+                        // 对每个角色对象的处理逻辑
+                        index++;
+                        var savePath = 'img/' + heroName + index + ".jpg";
+                        download(heroesPicUrl,savePath,downloadCallback);
+                    },
+                    function (err, result) {
+                        if(err){
+                            console.log("error is:"+err);
+                            finish();
+                            return;
+                        }
+                        //这里的result就是callback回来的数组
+                        console.log("下载完成，共计:"+result.length+"个");
+                        finish();
+                    }
+                );
+
+                function download(heroesPicUrl,savePath,downloadCallback){
+                    http.get(heroesPicUrl, function(res){
+                        var imgData = "";
+                        res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
+                        res.on("data", function(chunk){
+                            imgData += chunk;
+                        });
+                        res.on("end", function(){
+                            fs.writeFile(savePath, imgData, "binary", function(err){
+                                if(err){
+                                    console.log("save fail:",err);
+                                    downloadCallback(null,heroesPicUrl);
+                                    return;
+                                }
+                                console.log("save success");
+                                downloadCallback(null,heroesPicUrl);
+                            });
+                        });
+                    });
                 }
-                //callback后才会结束此并行“线程”
-                callback(null, hero);
+                function finish(){
+                    console.log('找到英雄:'+heroTitle+" "+heroName+"|"+heroType);
+                    concurrencyCount--;
+                    var hero = {
+                        succ:true,
+                        title:heroTitle,
+                        name:heroName,
+                        type:heroType,
+                        url:heroUrl
+                    }
+                    //callback后才会结束此并行“线程”
+                    callback(null, hero);
+                }
             }
         });
 }
